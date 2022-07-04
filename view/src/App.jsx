b46beeaf1ac3d646b562d803a1675e75e870a457
Vanessa Man <thepurplesnail@github.com>
2022-07-04 15:27:32 -0500

    Sidebar opens and closes

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -1,11 +1,17 @@
-import './css/App.css';
+import React, {useState} from 'react'
+import './css/App.css'
 import Sidebar from './sidebar'
-import { Outlet } from 'react-router-dom';
+import styled from 'styled-components'
+import { Outlet } from 'react-router-dom'
+import { StyledMenu } from './components/styling'
 
 function App() {
+  const [showSidebar, setShowSidebar] = useState(false);
+
   return (
     <div className="App">
-      <Sidebar/>
+      <Sidebar showSidebar = {showSidebar} onClick = {() => setShowSidebar(false)}/>
+      <StyledMenu onClick = {() => setShowSidebar(true)}/>
       <Outlet/>
     </div>
   );
--- view/src/sidebar.jsx
+++ view/src/sidebar.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from 'styled-components'
-import {NavLink} from 'react-router-dom'
 import { SidebarData } from './components/sidebar-data'
 import './css/sidebar.css'
-import { StyledLink } from './components/styling'
+import { StyledLink, StyledCaret, StyledSidebarWrap, StyledClose } from './components/styling'
 
 const Sidebar = props => {
-        
+    const [expanded, setExpanded] = useState(false);
+
     return(
-        <div className='sidebar'>
-            <ul>
-                {SidebarData.map((item, index) => 
-                    <li key = {index}>
-                        <StyledLink to = {item.path}>
-                            <div className='link-wrapper'>
-                                {item.icon}
-                                {item.title}
-                            </div>
-                        </StyledLink>
-                    </li>
-                )}
-            </ul>
-        </div>
+        <StyledSidebarWrap showSidebar = {props.showSidebar}>
+            <div className='close' onClick={props.onClick}><StyledClose/></div>
+            {SidebarData.map((item, index) => 
+                <StyledLink to = {item.path} key = {index}>
+                    <div className='link-wrapper'>
+                        {item.icon}
+                        {item.title}
+                        { item.subset &&
+                        <div onClick = {() => setExpanded(!expanded)}>
+                            <StyledCaret expanded = {expanded}/>
+                        </div>
+                        }
+                    </div>
+                </StyledLink>
+            )}
+        </StyledSidebarWrap>
     )
 }
 
-export default Sidebar
+export default Sidebar;
