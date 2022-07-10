507bf86f94c860e38053f3f5937250139676c072
JunLee365 <junholee365@gmail.com>
2022-07-10 00:39:55 -0400

    modified second GET route

--- server/server.js
+++ server/server.js
@@ -1,16 +1,21 @@
+// THIS IS ALL JUST A TEST [A refresher]
 // 1] dependencies
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+
+// 6] addittional dependencies for reading JSON
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 // 2] main
 app.listen(8000, function(){
-    console.log("server is running!");
+    console.log("server is running!")
 })
 
 // 3] test array
 const mockUserData=[
-    {name:'Mark'},
-    {name:'Jill'}
+    {name:'mark', age:18},
+    {name:'jill', age: 20}
 ]
 
 // 4] test GET request
@@ -24,11 +29,11 @@ app.get('/users', function(req, res){
 
 // 5] test GET id request
 app.get('/users/:id',function(req, res){
-	console.log(req.params.id)
+	let findName = req.params.id
+    console.log(findName)
 	res.json({
 		success: true,
 		message: 'got one user',
-		user: req.params.id // retrevies request parameter from URL :id
-                            // THIS DOES NOT GET DATA YET
+		user: mockUserData.find( ({ name }) => name === findName )
 	})
 })
