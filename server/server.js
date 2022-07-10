2019d8843f871729a4db614557cde71030f792ca
JunLee365 <junholee365@gmail.com>
2022-07-10 00:42:59 -0400

    add login POST route

--- server/server.js
+++ server/server.js
@@ -37,3 +37,29 @@ app.get('/users/:id',function(req, res){
 		user: mockUserData.find( ({ name }) => name === findName )
 	})
 })
+
+// 7] test POST request
+app.post('/login',function(req,res){
+	// Typically passwords are encrypted using something like bcrypt before sending to database
+	const username=req.body.username;
+	const password=req.body.password;
+
+	// This should come from the database
+	const mockUsername="billyTheKid";
+	const mockPassword="superSecret";
+
+	if (username===mockUsername && password===mockPassword){
+		// In practice, use JSON web token sign method here to make an encrypted token
+		res.json({
+			success: true,
+			message: 'password and username match!',
+			token: 'encrypted token goes here'
+		})
+	} else {
+		res.json({
+			success: false,
+			message: 'password and username do not match'
+		})
+	}
+
+})
